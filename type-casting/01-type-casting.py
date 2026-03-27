d8fbc46c2f398fb884701601585ea07f5aae19f7
Hammad Ali Khan <hammad.web3.0developer@gmail.com>
2026-03-27 10:39:13 -0700

    01: type-casting

---
+++ type-casting/01-type-casting.py
@@ -0,0 +1,25 @@
+
+addition_number_1=float(input())
+addition_number_2=float(input())
+oper = input()
+print("Sum of two number is:",addition_number_1+addition_number_2)
+
+subtraction_number_1=float(input())
+subtraction_number_2=float(input())
+oper = input()
+print("Subtraction of two number is:",subtraction_number_1-subtraction_number_2)
+
+multipication_number_1=float(input())
+multipiction_number_2=float(input())
+oper = input()
+print("Sum of two number is:",multipication_number_1*multipiction_number_2)
+
+division_number_1=float(input())
+division_number_2=float(input())
+oper = input()
+print("Divide of two number is:",division_number_1/division_number_2)
+
+
+
+
+
